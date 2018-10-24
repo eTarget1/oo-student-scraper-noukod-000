c05673646c21315ffc70e3554c27e03876f815a1
clifford <clifford@noukod.com>
2018-10-24 14:11:42 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,26 +14,20 @@ class Scraper
          :profile_url => student.css('a').attribute('href').value
      }
     end
-  end	
+  end
 
   def self.scrape_profile_page(profile_url)
-    profile_html = open(profile_url)
-    profile_doc = Nokogiri::HTML(profile_html)
-    attributes = {}
-    profile_doc.css("div.social-icon-container a").each do |link_xml|
-      case link_xml.attribute("href").value
-      when /twitter/
-        attributes[:twitter] = link_xml.attribute("href").value
-      when /github/
-        attributes[:github] = link_xml.attribute("href").value
-      when /linkedin/
-        attributes[:linkedin] = link_xml.attribute("href").value
-      else
-          attributes[:blog] = link_xml.attribute("href").value
-      end
-    end
-    attributes[:profile_quote] = profile_doc.css("div.profile-quote").text
-    attributes[:bio] = profile_doc.css("div.bio-content div.description-holder").text.strip
-    attributes
-  end
+    doc = Nokogiri::HTML(open(profile_url))
+     profile = {}
+     profile[:profile_quote] = doc.css('div.profile-quote').text
+     profile[:bio] = doc.css('div.bio-content div.description-holder p').text
+     social = doc.css('div.social-icon-container a')
+     s_name = %w(twitter github linkedin)
+     social.each do |link|
+       name = link.attr('href').split('/')[2].gsub('.com', '').gsub('www.', '')
+       s_name.include?(name) ? name : name = 'blog'
+       profile[name.to_sym] = link.attr('href')
+     end
+     profile
+   end
 end
