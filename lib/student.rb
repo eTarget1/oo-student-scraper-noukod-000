10b2f9756949a608d3dec0ae655225caac42b521
clifford <clifford@noukod.com>
2018-10-24 14:22:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,9 @@ class Student
 
   def add_student_attributes(attributes_hash)
     	    attributes_hash.each {|key, value| self.send("#{key}=", value)}
-  end	  
-
+  end
+  def self.all
+     	    @@all
+   end	  end
 
 end
