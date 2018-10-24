965a8dccce039e1a4d897aab90566214ee0fdd05
clifford <clifford@noukod.com>
2018-10-24 14:42:38 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,33 +1,59 @@
+Scraper
 require 'open-uri'
 require 'pry'
-require 'Nokogiri'
+require 'nokogiri'
 
 class Scraper
-
+  attr_accessor :students
   def self.scrape_index_page(index_url)
-         doc = Nokogiri::HTML(open(index_url))
-   students_scrape = doc.css('div.student-card')
-    students_scrape.collect do |student|
-     {
-         :name => student.css('h4').text,
-         :location => student.css('p').text,
-         :profile_url => student.css('a').attribute('href').value
-     }
-    end
+    html = open(index_url)
+       list = Nokogiri::HTML(html)
+
+       names = list.css(".student-name")
+       names_array = []
+       names.each do |item|
+         names_array << item.text
+       end
+       names_array
+       locations = list.css(".student-location")
+       location_array = []
+       locations.each do |item|
+         location_array << item.text
+       end
+       location_array
+       webpages = list.css(".student-card a[href]")
+       webpage_array = []
+       webpages.select do |item|
+         webpage_array << item['href']
+       end
+       webpage_array
+       master_array = []
+       x = 0
+       names_array.each do |name|
+         master_array << {:name => name, :location => location_array[x], :profile_url => webpage_array[x]}
+         x += 1
+       end
+       master_array
+     end
+  def self.scrape_profile_page(profile_url)
+    profile_html = open(profile_url)
+        profile_doc = Nokogiri::HTML(profile_html)
+        attributes = {}
+        profile_doc.css("div.social-icon-container a").each do |link_xml|
+          case link_xml.attribute("href").value
+          when /twitter/
+            attributes[:twitter] = link_xml.attribute("href").value
+          when /github/
+            attributes[:github] = link_xml.attribute("href").value
+          when /linkedin/
+            attributes[:linkedin] = link_xml.attribute("href").value
+          else
+              attributes[:blog] = link_xml.attribute("href").value
+          end
+        end
+        attributes[:profile_quote] = profile_doc.css("div.profile-quote").text
+        attributes[:bio] = profile_doc.css("div.bio-content div.description-holder").text.strip
+        attributes
   end
 
-  def self.scrape_profile_page(profile_url)
-    doc = Nokogiri::HTML(open(profile_url))
-     profile = {}
-     profile[:profile_quote] = doc.css('div.profile-quote').text
-     profile[:bio] = doc.css('div.bio-content div.description-holder p').text
-     social = doc.css('div.social-icon-container a')
-     s_name = %w(twitter github linkedin)
-     social.each do |link|
-       name = link.attr('href').split('/')[2].gsub('.com', '').gsub('www.', '')
-       s_name.include?(name) ? name : name = 'blog'
-       profile[name.to_sym] = link.attr('href')
-     end
-     profile
-   end
 end
